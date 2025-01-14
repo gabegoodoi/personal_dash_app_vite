3aefc42533a84c69dde3e85ffd4f305e9c5b3eaf
Gabe Godoi <gabegodoi@gmail.com>
2025-01-13 18:34:29 -0800

    change yet again x17 to yml

--- src/Tests/location.test.tsx
+++ src/Tests/location.test.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from 'react';  // Ensuring React is imported for type annotations
 import { render, screen } from '@testing-library/react';
 import Location from '../Components/Location/Location.tsx';
 import '@testing-library/jest-dom';
+
+// Arbitrary use of React import
+const _unusedReact: typeof React = React;
+console.log(_unusedReact)
 
 test('renders the location name when data is fetched', async () => {
     render(<Location />);
